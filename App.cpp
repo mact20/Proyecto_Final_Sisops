e544b1ee897b28153de95bf277e8c2a045659cd1
mact20 <61257611+mact20@users.noreply.github.com>
2021-05-30 14:42:50 -0500

    Declaracion de variables

--- App.cpp
+++ App.cpp
@@ -3,6 +3,20 @@
 #include <string>
 using namespace std;
 
+/*
+Mauricio Alejandro Garza Martínez A00825042
+Lourdes Montserrat Cruz Castillo A01283101
+Marco Aurelio Cepeda Treviño A00824947
+*/
+
 
 int main() {
+  //Se va implementar un menu de opciones para ver el control de este logaritmo
+  string alg; //string que va a guardar el nombre del algoritmo
+  char letras[10]={'A','B','C','D','E','F','G','H','I','J'}; //Procesos
+  char solicitudes[30]; //Lista de solicitudes
+  char marcos[5]; //Aqui se van a asignar los marcos
+  int nMarcos=5; //No. de marcos disponibles
+  queue<char> marcoFIFO; //Queue que nos ayudara en la implementacion FIFO
+  char marcoLRU[30]; //Arreglo aud nox ayudara en la implementacion de LRU
 }
