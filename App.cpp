a00d794ea4f0a3a20346710d1df36ec92fa8a81e
montsecc00 <montsecc@live.com.mx>
2021-05-31 23:30:46 -0500

    Genera Solicitudes
    Se genera el arreglo de 30 solicitudes de procesos

--- App.cpp
+++ App.cpp
@@ -9,7 +9,6 @@ Lourdes Montserrat Cruz Castillo A01283101
 Marco Aurelio Cepeda Treviño A00824947
 */
 
-
 int main() {
   //Se va implementar un menu de opciones para ver el control de este logaritmo
   string alg; //string que va a guardar el nombre del algoritmo
@@ -18,5 +17,13 @@ int main() {
   char marcos[5]; //Aqui se van a asignar los marcos
   int nMarcos=5; //No. de marcos disponibles
   queue<char> marcoFIFO; //Queue que nos ayudara en la implementacion FIFO
-  char marcoLRU[30]; //Arreglo aud nox ayudara en la implementacion de LRU
+  char marcoLRU[30]; //Arreglo aud nox ayudara en la implementacion de LR
+  
+  for(int i=0;i<30;i++){
+    solicitudes[i]= letras[rand() % 10];//Al arreglo solicitudes se le asigna una letra random del arreglo letras
+  }
+  //for(int i=0;i<30;i++){
+  //  cout << solicitudes[i]; //Se imprime el arreglo de 30 letras
+  //}
+  
 }
